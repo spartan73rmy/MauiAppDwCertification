3698601739dc3c2d366cada282976a2eccef97a5
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2023-07-24 18:08:16 -0600

    Initial

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppDWCert;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppDWCert;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
