1c36efc08374ad9e2e7b36cb1b51bf5b46e95b35
Alberto Espinoza Morelos <aespinoza@giddingsfruit.com>
2023-07-26 20:56:53 -0600

    MVVM

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,19 +1,68 @@
-﻿using SQLite;
+﻿using AppDWCert.ViewModels.Base;
+using SQLite;
 
 namespace AppDWCert.Models
 {
-    public class Car
+    public class Car : ObservableObject
     {
+        int id;
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Year { get; set; }
-        public string PhotoUrl { get; set; }
+        public int Id { get => id; set => SetProperty(ref id, value); }
 
-        public double? Lat { get; set; }
-        public double? Lon { get; set; }
+        private string brand;
+        public string Brand
+        {
+            get => brand;
+            set => SetProperty(ref brand, value);
+        }
+
+        private string model;
+        public string Model
+        {
+            get => model;
+            set => SetProperty(ref model, value);
+        }
+
+        private string description;
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get => price;
+            set => SetProperty(ref price, value);
+        }
+
+        private int year;
+        public int Year
+        {
+            get => year;
+            set => SetProperty(ref year, value);
+        }
+
+        private string photoUrl;
+        public string PhotoUrl
+        {
+            get => photoUrl;
+            set => SetProperty(ref photoUrl, value);
+        }
+
+        private double? lat;
+        public double? Lat
+        {
+            get => lat;
+            set => SetProperty(ref lat, value);
+        }
+
+        private double? lon;
+        public double? Lon
+        {
+            get => lon;
+            set => SetProperty(ref lon, value);
+        }
     }
 }
---
+++ ViewModels/Base/ObservableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace AppDWCert.ViewModels.Base
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> property)
+        {
+            var name = GetMemberInfo(property).Name;
+            OnPropertyChanged(name);
+        }
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        private MemberInfo GetMemberInfo(Expression expression)
+        {
+            MemberExpression operand;
+            LambdaExpression lambdaExpression = (LambdaExpression)expression;
+            if (lambdaExpression.Body as UnaryExpression != null)
+            {
+                UnaryExpression body = (UnaryExpression)lambdaExpression.Body;
+                operand = (MemberExpression)body.Operand;
+            }
+            else
+            {
+                operand = (MemberExpression)lambdaExpression.Body;
+            }
+            return operand.Member;
+        }
+    }
+}
