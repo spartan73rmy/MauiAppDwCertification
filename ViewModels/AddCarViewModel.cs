8f9ba549c7e68ad9b8ad13e04e0e8168082cc367
Alberto Espinoza Morelos <aespinoza@giddingsfruit.com>
2023-07-27 18:39:58 -0600

    Add view model base

---
+++ ViewModels/AddCarViewModel.cs
@@ -0,0 +1,51 @@
+﻿using AppDWCert.Context;
+using AppDWCert.Models;
+using AppDWCert.ViewModels.Base;
+using System.Windows.Input;
+
+namespace AppDWCert.ViewModels
+{
+    public class AddCarViewModel : BaseViewModel
+    {
+        public Car CarModel { get; set; }
+        public AddCarViewModel(INavigation navigation) : base(navigation)
+        {
+            CarModel = new Car();
+        }
+
+        //Comandos
+        public ICommand TakePhotoCommand => new Command(async () => await TakePhotoAsync());
+        public ICommand AddCarCommand => new Command(async () => await AddCarAsync());
+
+        private async Task TakePhotoAsync()
+        {
+            var photo = await MediaPicker.Default.CapturePhotoAsync(new MediaPickerOptions());
+            CarModel.PhotoUrl = photo.FullPath;
+
+            var photoStream = await photo.OpenReadAsync();
+        }
+
+        private async Task AddCarAsync()
+        {
+            var location = await Geolocation.Default.GetLocationAsync();
+            await Navigation.PopAsync();
+
+            PermissionStatus status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            if (status == PermissionStatus.Denied)
+            {
+                //await DisplayAlert("Permiso", "La aplicacion requiere la ubicacion para guardar el carro", "Ok");
+                return;
+            }
+
+            if (location is null)
+                return;
+
+            CarModel.Lat = location.Latitude;
+            CarModel.Lon = location.Longitude;
+            CarModel.PhotoUrl = "https://static.wikia.nocookie.net/disney/images/6/6f/Profile_-_Maui.jpeg/revision/latest/thumbnail/width/360/height/360?cb=20190628013307";
+
+            await new RestService().Set("CarsForSalesApi", CarModel);
+            _ = await Navigation.PopAsync(true);
+        }
+    }
+}
--- Views/AddCar.xaml.cs
+++ Views/AddCar.xaml.cs
@@ -1,5 +1,4 @@
-using AppDWCert.Context;
-using AppDWCert.Models;
+using AppDWCert.ViewModels;
 
 namespace AppDWCert.Views;
 
@@ -8,35 +7,6 @@ public partial class AddCar : ContentPage
     public AddCar()
     {
         InitializeComponent();
-    }
-
-    private async void btnAceptar_Clicked(object sender, EventArgs e)
-    {
-        var location = await Geolocation.Default.GetLocationAsync();
-
-        PermissionStatus status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-        if (status == PermissionStatus.Denied)
-        {
-            await DisplayAlert("Permiso", "La aplicacion requiere la ubicacion para guardar el carro", "Ok");
-            return;
-        }
-
-        if (location is null)
-            return;
-
-        var car = new Car()
-        {
-            Brand = EntryMarca.Text,
-            Model = EntryModelo.Text,
-            Description = EntryDescripcion.Text,
-            Year = int.Parse(EntryYear.Text),
-            Price = decimal.Parse(EntryPrecio.Text),
-            Lat = location.Latitude,
-            Lon = location.Longitude,
-            PhotoUrl = "https://static.wikia.nocookie.net/disney/images/6/6f/Profile_-_Maui.jpeg/revision/latest/thumbnail/width/360/height/360?cb=20190628013307"
-        };
-
-        await new RestService().Set("CarsForSalesApi", car);
-        _ = await Navigation.PopAsync(true);
+        BindingContext = new AddCarViewModel(Navigation);
     }
 }
